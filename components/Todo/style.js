90d17cfb3197e0cc284e5062ed8532d70df312c5
Cullen Middleton <culmid@gmail.com>
2022-05-23 14:15:09 +0200

    Create the Todo component

---
+++ components/Todo/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  todoContainer: {
+    minHeight: 100,
+    width: "100%",
+    borderColor: "#000",
+    borderWidth: 2,
+  },
+});
